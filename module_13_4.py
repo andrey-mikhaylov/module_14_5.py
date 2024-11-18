513473d04485ce04790d60340bf8c4e0b5eac964
Andrey <andrey.mikhaylov.n@gmail.com>
2024-11-18 14:03:20 +0300

    done

--- module_13_4.py
+++ module_13_4.py
@@ -1,11 +1,109 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+import asyncio
+
+try:
+    from credentials import token
+except ImportError:
+    print("Error: You must use your own Telegram Bot API token to use this program")
+    print("Place token in credentials.py")
+    exit(1)
+
+
+def calc_calories(gender: str, age: float, growth: float, weight: float):
+    """
+    формула Миффлина - Сан Жеора для подсчёта нормы калорий
+    # (для женщин или мужчин - на ваше усмотрение).
+    :param gender: пол, 'M' или 'F'
+    :param age: вес в кг
+    :param growth: рост в см
+    :param weight: возраст в г
+    :return: норма калорий
+    """
+    # (10 х вес в кг) + (6, 25 х рост в см) – (5 х возраст в г) + 5(M) или -161(F).
+    return (10.0 * weight) + (6.25 * growth) - (5.0 * age) + 5.0 if gender == 'M' else -161.0
+
+
+class UserState(StatesGroup):
+    # Создайте класс UserState наследованный от StatesGroup.
+    # Внутри этого класса опишите 3 объекта класса State: age, growth, weight (возраст, рост, вес).
+    # Эта группа(класс) будет использоваться в цепочке вызовов message_handler'ов.
+    age = State()
+    growth = State()
+    weight = State()
+
+
+bot = Bot(token=token)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+
+@dp.message_handler(text='Calories')
+async def set_age(message):
+    # Эта функция должна выводить в Telegram-бот сообщение 'Введите свой возраст:'.)
+    await message.answer('Введите свой возраст:')
+    # После ожидать ввода возраста в атрибут UserState.age при помощи метода set.
+    await UserState.age.set()
+
+
+@dp.message_handler(state = UserState.age)
+async def set_growth(message, state):
+    # Эта функция должна обновлять данные в состоянии age на message.text
+    await state.update_data(age=message.text)
+    # Далее должна выводить в Telegram-бот сообщение 'Введите свой рост:'.
+    await message.answer('Введите свой рост:')
+    # После ожидать ввода роста в атрибут UserState.growth при помощи метода set.
+    await UserState.growth.set()
+
+
+@dp.message_handler(state = UserState.growth)
+async def set_weight(message, state):
+    # Эта функция должна обновлять данные в состоянии growth на message.text
+    await state.update_data(growth=message.text)
+    # Далее должна выводить в Telegram-бот сообщение 'Введите свой вес:'.
+    await message.answer('Введите свой вес:')
+    # После ожидать ввода роста в атрибут UserState.weight при помощи метода set.
+    await UserState.weight.set()
+
+
+@dp.message_handler(state = UserState.weight)
+async def send_calories(message, state: State.state):
+    # Эта функция должна обновлять данные в состоянии weight на message.text (написанное пользователем сообщение). Используйте метод update_data.
+    await state.update_data(weight=message.text)
+    # Далее в функции запомните в переменную data все ранее введённые состояния
+    data = await state.get_data()
+    age, growth, weight = (float(data[k]) for k in ['age', 'growth', 'weight'])
+    # Используйте упрощённую формулу Миффлина - Сан Жеора для подсчёта нормы калорий
+    calories = calc_calories('M', age, growth, weight)
+    # Результат вычисления по формуле отправьте ответом пользователю в Telegram-бот.
+    await message.answer(f'Ваша норма калорий: {calories}')
+    # Финишируйте машину состояний методом finish().
+    await state.finish()
+
+
+@dp.message_handler(commands=['start'])
+async def start_message(message):
+    """ обработчик команды start """
+    # печатает строку в консоли 'Привет! Я бот помогающий твоему здоровью.' .
+    # Запускается только когда написана команда '/start' в чате с ботом.
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+
+
+@dp.message_handler()
+async def all_messages(message):
+    """ обработчик остальных сообщений """
+    # печатает строку в консоли 'Введите команду /start, чтобы начать общение.'.
+    # Запускается при любом обращении не описанном ранее.
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
 
 def main():
-    pass
+    executor.start_polling(dp, skip_updates=True)
 
 
 if __name__ == '__main__':
     main()
-
 
 """
 2024/01/20 00:00|Домашнее задание по теме "Машина состояний".
